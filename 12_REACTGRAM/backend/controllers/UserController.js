92fb826cfc8f5a179d256e8e41df848615260041
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-19 14:14:22 -0300

    Updating user

--- 12_REACTGRAM/backend/controllers/UserController.js
+++ 12_REACTGRAM/backend/controllers/UserController.js
@@ -2,6 +2,8 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const mongoose = require('mongoose')
+
 const jwtSecret = process.env.JWT_SECRET;
 
 // Gerando token do usuário
@@ -88,7 +90,42 @@ const getCurrentUser = async(req, res) =>{
 
 // Update  an user
 const update = async(req, res) =>{
-    res.send('Update')
+    
+    const {name, password, bio} = req.body
+
+    let profileImage = null
+
+    if(req.file){
+        profileImage = req.file.filename
+    }
+
+    const reqUser = req.user
+
+    const user = await User.findById(reqUser._id).select('-password');
+
+    if(name){
+        user.name = name
+    }
+
+    if(password){
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        user.password = passwordHash
+    }
+
+    if(profileImage){
+        user.profileImage = profileImage
+    }
+
+    if(bio){
+        user.bio = bio
+    }
+
+    await user.save()
+
+    res.status(200).json(user)
+
 }
 
 module.exports = {
