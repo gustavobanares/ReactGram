486242f77a9ec3d96b071c5a63e5da46761afe90
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-22 21:05:45 -0300

    Create navbar

--- 12_REACTGRAM/frontend/src/App.jsx
+++ 12_REACTGRAM/frontend/src/App.jsx
@@ -3,10 +3,15 @@ import './App.css'
 // Router
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
+// Components
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 // Pages
 import Home from './pages/Home/Home'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
+
 
 function App() {
   
@@ -14,11 +19,13 @@ function App() {
     
       <div>
         <BrowserRouter>
+          <Navbar />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
       </div>
       
