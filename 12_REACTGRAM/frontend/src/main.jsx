7d7de7d736c3bc232ff2397904b1e6baf1e002d0
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-23 21:50:34 -0300

    Implementing Redux

--- 12_REACTGRAM/frontend/src/main.jsx
+++ 12_REACTGRAM/frontend/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Redux 
+import {Provider} from 'react-redux'
+import {store} from './store'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
