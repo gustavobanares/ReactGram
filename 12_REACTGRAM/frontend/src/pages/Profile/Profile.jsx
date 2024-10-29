af48754d96c46bfd88fba4bfaa10f2b4ae8a724a
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-28 21:42:44 -0300

    Displaying photos on the user profile

--- 12_REACTGRAM/frontend/src/pages/Profile/Profile.jsx
+++ 12_REACTGRAM/frontend/src/pages/Profile/Profile.jsx
@@ -1,114 +1,130 @@
-import './Profile.css'
+import './Profile.css';
 
-import {uploads} from '../../utils/config'
+import { uploads } from '../../utils/config';
 
 // components
-import Message from '../../components/Message'
-import { Link } from 'react-router-dom'
-import { BsFillEyeFill, BsPencilFill, BsXLg } from 'react-icons/bs'
+import Message from '../../components/Message';
+import { Link } from 'react-router-dom';
+import { BsFillEyeFill, BsPencilFill, BsXLg } from 'react-icons/bs';
 
 // hooks
-import {useState, useEffect, useRef} from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useState, useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 // redux
-import { getUserDetails } from '../../slices/userSlice'
-import { publishPhoto, resetMessage } from '../../slices/photoSlice'
+import { getUserDetails } from '../../slices/userSlice';
+import { publishPhoto, resetMessage, getUserPhotos } from '../../slices/photoSlice';
 
 const Profile = () => {
+    const { id } = useParams();
+    const dispatch = useDispatch();
 
-    const {id} = useParams()
+    const { user = {}, loading } = useSelector((state) => state.user);
+    const { user: userAuth } = useSelector((state) => state.auth);
+    const { photos = [], loading: loadingPhoto, message: messagePhoto, error: errorPhoto } = useSelector((state) => state.photo);
 
-    const dispatch = useDispatch()
-
-    const {user, loading} = useSelector((state) => state.user)
-    const {user: userAuth} = useSelector((state) => state.auth)
-    const {photos, loading: loadingPhoto, message: messagePhoto, error: errorPhoto } = useSelector((state) => state.photo)
-
-    const [title, setTitle] = useState('')
-    const [image, setImage] = useState('')
+    const [title, setTitle] = useState('');
+    const [image, setImage] = useState('');
 
     // New form and edit forms refs
-    const newPhotoForm = useRef()
-    const editPhotoForm = useRef()
+    const newPhotoForm = useRef();
 
     // Load user data
-    useEffect(() =>{
-
-        dispatch(getUserDetails(id))
-
-    }, [dispatch, id])
+    useEffect(() => {
+        dispatch(getUserDetails(id));
+        dispatch(getUserPhotos(id));
+    }, [dispatch, id]);
 
     const handleFile = (e) => {
-        const image = e.target.files[0];
-    
-        setImage(image);
-      };
+        const selectedImage = e.target.files[0];
+        setImage(selectedImage);
+    };
 
-    const submitHandle = (e) =>{
-        e.preventDefault()
+    const submitHandle = (e) => {
+        e.preventDefault();
 
-        const photoData = {
-            title,
-            image
-        }
+        const photoData = { title, image };
 
         // build form data
-        const formData = new FormData()
+        const formData = new FormData();
+        Object.keys(photoData).forEach((key) => formData.append(key, photoData[key]));
 
-        const photoFormData = Object.keys(photoData).forEach((key) => formData.append(key, photoData[key]))
+        dispatch(publishPhoto(formData));
 
-        formData.append('photo', photoFormData)
-
-        dispatch(publishPhoto(formData))
-
-        setTitle('')
+        setTitle('');
 
         setTimeout(() => {
             dispatch(resetMessage());
-          }, 2000);
+        }, 2000);
+    };
 
-    }
-
-    if(loading) {
-        return <p>Carregando...</p>
+    if (loading) {
+        return <p>Carregando...</p>;
     }
 
     return (
         <div id="profile">
-          <div className="profile-header">
-            {user?.profileImage && (
-              <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
-            )}
-            <div className="profile-description">
-              <h2>{user?.name}</h2>
-              <p>{user?.bio}</p>
-            </div>
-          </div>
-          {id === userAuth._id &&(
-            <>
-                <div className="new-photo" ref={newPhotoForm}>
-                    <h3>Compartilhe algum momento seu:</h3>
-                    <form onSubmit={submitHandle}>
-                        <label>
-                            <span>Título para a foto:</span>
-                            <input type="text" placeholder='Insira um título' onChange={(e) => setTitle(e.target.value)} value={title || ''} />
-                        </label>
-                        <label>
-                            <span>Imagem:</span>
-                            <input type="file" onChange={handleFile} />
-                        </label>
-                        {!loadingPhoto && <input type="submit" value="Postar" />}
-                        {loadingPhoto && <input type="submit" disabled value="Aguarde..." />}
-                    </form>
+            <div className="profile-header">
+                {user?.profileImage && (
+                    <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+                )}
+                <div className="profile-description">
+                    <h2>{user?.name}</h2>
+                    <p>{user?.bio}</p>
                 </div>
-                {errorPhoto && <Message msg={errorPhoto} type='error' />}
-                {messagePhoto && <Message msg={messagePhoto} type='sucess' />}
-            </>
-          )}
+            </div>
+            {id === userAuth._id && (
+                <>
+                    <div className="new-photo" ref={newPhotoForm}>
+                        <h3>Compartilhe algum momento seu:</h3>
+                        <form onSubmit={submitHandle}>
+                            <label>
+                                <span>Título para a foto:</span>
+                                <input type="text" placeholder='Insira um título' onChange={(e) => setTitle(e.target.value)} value={title || ''} />
+                            </label>
+                            <label>
+                                <span>Imagem:</span>
+                                <input type="file" onChange={handleFile} />
+                            </label>
+                            {!loadingPhoto ? (
+                                <input type="submit" value="Postar" />
+                            ) : (
+                                <input type="submit" disabled value="Aguarde..." />
+                            )}
+                        </form>
+                    </div>
+                    {errorPhoto && <Message msg={errorPhoto} type='error' />}
+                    {messagePhoto && <Message msg={messagePhoto} type='success' />}
+                </>
+            )}
+            <div className='user-photos'>
+                <h2>Fotos publicadas:</h2>
+                <div className="photos-container">
+                    {loadingPhoto ? (
+                        <p>Carregando fotos...</p>
+                    ) : (
+                        photos.length > 0 ? (
+                            photos.map((photo) => (
+                                <div className="photo" key={photo._id}>
+                                    {photo.image && (
+                                        <img src={`${uploads}/photos/${photo.image}`} alt={photo.title} />
+                                    )}
+                                    {id === userAuth._id ? (
+                                        <p>actions</p>
+                                    ) : (
+                                        <Link className="btn" to={`/photos/${photo._id}`}>Ver</Link>
+                                    )}
+                                </div>
+                            ))
+                        ) : (
+                            <p>Nenhuma foto publicada ainda.</p>
+                        )
+                    )}
+                </div>
+            </div>
         </div>
-      );
-}
+    );
+};
 
-export default Profile
+export default Profile;
