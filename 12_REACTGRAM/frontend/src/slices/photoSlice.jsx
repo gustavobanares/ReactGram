ce08a12e8ab2201183efdbb83b172cfd9f59cae5
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-30 18:25:17 -0300

    Photo like slice

--- 12_REACTGRAM/frontend/src/slices/photoSlice.jsx
+++ 12_REACTGRAM/frontend/src/slices/photoSlice.jsx
@@ -81,13 +81,30 @@ export const updatePhoto = createAsyncThunk(
   }
 )
 
-// Get poto by id
+// Get photo by id
 export const getPhoto = createAsyncThunk('photo/getphoto', async(id, thunkAPI) =>{
   const token = thunkAPI.getState().auth.user.token
 
   const data = await photoService.getPhoto(id, token)
 
   return data
+})
+
+// Like a photo
+export const like = createAsyncThunk('photo/like', async(id, thunkAPI) =>{
+
+  const token = thunkAPI.getState().auth.user.token
+
+  const data = await photoService.like(id, token)
+
+   // Check for errors
+   if(data.errors){
+    return thunkAPI.rejectWithValue(data.errors[0])
+  }
+
+  return data
+
+
 })
 
 // Thunk para buscar todas as fotos
@@ -205,6 +222,28 @@ export const photoSlice = createSlice({
         state.success = true;
         state.error = null;
         state.photo = action.payload;
+      })
+      .addCase(like.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+
+        if(state.photo.likes){
+          state.photo.likes.push(action.payload.userId)
+        }
+        
+        state.photos.map((photo) =>{
+          if(photo.id === action.payload.photo.photoId) {
+            return photo.likes.push(action.payload.userId)
+          }
+          return photo
+        })
+
+        state.message = action.payload.message
+      })
+      .addCase(like.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
       })
 }
 })
