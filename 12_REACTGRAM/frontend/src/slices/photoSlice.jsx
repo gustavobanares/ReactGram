75085cf37353c2668672500a6091f9de38e72f91
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-31 19:29:27 -0300

    Slice all photos

--- 12_REACTGRAM/frontend/src/slices/photoSlice.jsx
+++ 12_REACTGRAM/frontend/src/slices/photoSlice.jsx
@@ -148,6 +148,15 @@ export const createPhoto = createAsyncThunk(
   }
 );
 
+// Get all photos
+export const getPhotos = createAsyncThunk('photo/getall', async() =>{
+
+  const data = await photoService.getPhotos()
+
+  return data
+
+})
+
 export const photoSlice = createSlice({
   name: 'photo',
   initialState,
@@ -272,6 +281,16 @@ export const photoSlice = createSlice({
       .addCase(comment.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
+      })
+      .addCase(getPhotos.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(getPhotos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos = action.payload;
       })
 }
 })
