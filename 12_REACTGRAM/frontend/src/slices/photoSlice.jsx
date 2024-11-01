7fbb1d26819f7a210be39464409c05822727a93e
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-11-01 01:12:39 -0300

    Search slice

--- 12_REACTGRAM/frontend/src/slices/photoSlice.jsx
+++ 12_REACTGRAM/frontend/src/slices/photoSlice.jsx
@@ -159,6 +159,15 @@ export const getPhotos = createAsyncThunk('photo/getall', async(_, thunkAPI) =>{
 
 })
 
+// Search photo by title
+export const searchPhotos = createAsyncThunk('photo/search', async(query, thunkAPI)  =>{
+  const token = thunkAPI.getState().auth.user.token
+
+  const data = await photoService.searchPhotos(query, token)
+
+  return data
+})
+
 export const photoSlice = createSlice({
   name: 'photo',
   initialState,
@@ -289,6 +298,16 @@ export const photoSlice = createSlice({
         state.error = false;
       })
       .addCase(getPhotos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos = action.payload;
+      })
+      .addCase(searchPhotos.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(searchPhotos.fulfilled, (state, action) => {
         state.loading = false;
         state.success = true;
         state.error = null;
